7a421d2751b76a77014c74de1fb9380989304b9e
TimelordUK <sjames8888@gmail.com>
2025-09-06 11:41:23 +0100

    fix: correct function names in physics_constants.sql
    - Use C() instead of SPEED_OF_LIGHT()
    - Use H() instead of PLANCK_CONSTANT()
    - Use K() instead of BOLTZMANN_CONSTANT()
    - Use AVOGADRO() instead of AVOGADRO_NUMBER()
    - Use MP() and MN() instead of PE() and NE()
    - All physics constant examples now working (5/5 queries successful)

--- examples/physics_constants.sql
+++ examples/physics_constants.sql
@@ -3,11 +3,12 @@
 
 -- Fundamental physics constants
 SELECT 
-    SPEED_OF_LIGHT() as c_meters_per_sec,
-    PLANCK_CONSTANT() as h_joule_seconds,
-    GRAVITATIONAL_CONSTANT() as G_newton_constant,
-    AVOGADRO_NUMBER() as avogadro_const,
-    BOLTZMANN_CONSTANT() as k_boltzmann;
+    C() as speed_of_light_ms,
+    H() as planck_constant_js,
+    G() as gravitational_constant,
+    AVOGADRO() as avogadro_number,
+    K() as boltzmann_constant_jk;
+GO
 
 -- Astronomical masses (in kg)
 SELECT 
@@ -16,26 +17,30 @@ SELECT
     MASS_MOON() as moon_mass_kg,
     MASS_JUPITER() as jupiter_mass_kg,
     MASS_MARS() as mars_mass_kg;
+GO
 
 -- Particle masses
 SELECT 
-    ELECTRON_MASS() as electron_kg,
-    PROTON_MASS() as proton_kg,
-    NEUTRON_MASS() as neutron_kg;
+    ME() as electron_mass_kg,
+    MP() as proton_mass_kg,
+    MN() as neutron_mass_kg;
+GO
 
 -- Practical calculations using constants
 SELECT 
     -- Calculate escape velocity from Earth: v = sqrt(2GM/r)
-    SQRT(2 * GRAVITATIONAL_CONSTANT() * MASS_EARTH() / 6371000) as earth_escape_velocity_ms,
+    SQRT(2 * G() * MASS_EARTH() / 6371000) as earth_escape_velocity_ms,
     
     -- Energy equivalent of 1kg mass: E = mc²
-    1 * POWER(SPEED_OF_LIGHT(), 2) as energy_joules_from_1kg,
+    1 * POWER(C(), 2) as energy_joules_from_1kg,
     
     -- Schwarzschild radius of the Sun: r = 2GM/c²
-    (2 * GRAVITATIONAL_CONSTANT() * MASS_SUN()) / POWER(SPEED_OF_LIGHT(), 2) as sun_schwarzschild_radius_m;
+    (2 * G() * MASS_SUN()) / POWER(C(), 2) as sun_schwarzschild_radius_m;
+GO
 
 -- Compare celestial bodies
 SELECT 
     MASS_JUPITER() / MASS_EARTH() as jupiter_earth_mass_ratio,
     MASS_SUN() / MASS_EARTH() as sun_earth_mass_ratio,
     MASS_EARTH() / MASS_MOON() as earth_moon_mass_ratio;
+GO
