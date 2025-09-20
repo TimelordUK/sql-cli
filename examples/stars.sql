f55b7b8eb6b8eb07882f0146a3225b352923add7
TimelordUK <sjames8888@gmail.com>
2025-09-20 18:15:09 +0100

    improve the stars.sql

--- examples/stars.sql
+++ examples/stars.sql
@@ -32,40 +32,43 @@ SELECT
     designation,
     distance_ly,
     constellation,
-    LAG(designation) OVER (ORDER BY distance_ly) AS prev_star,
-    LAG(distance_ly) OVER (ORDER BY distance_ly) AS prev_distance,
-    LAG(constellation) OVER (ORDER BY distance_ly) AS prev_constellation,
-    ROUND(distance_ly - LAG(distance_ly) OVER (ORDER BY distance_ly), 2) AS distance_delta
+    LAG(designation) OVER (ORDER BY distance_ly ASC) AS prev_star,
+    LAG(distance_ly) OVER (ORDER BY distance_ly ASC) AS prev_distance,
+    LAG(constellation) OVER (ORDER BY distance_ly ASC) AS prev_constellation,
+    ROUND(distance_ly - LAG(distance_ly) OVER (ORDER BY distance_ly ASC), 2) AS distance_delta
 FROM star_systems_20_25_ly
-ORDER BY distance_ly;
+ORDER BY distance_ly ASC;
 GO
 
 -- Query 6: Partition stars into 5 light-year bands and rank within each band
 -- Shows concentration of stars in distance ranges
 -- Note: Using a pre-calculated band column since PARTITION BY doesn't support CASE expressions yet
-WITH distance_bands AS (
-    SELECT
-        designation,
-        distance_ly,
-        constellation,
-        stellar_class,
-        CASE
-            WHEN distance_ly < 15 THEN '10-15 ly'
-            WHEN distance_ly < 20 THEN '15-20 ly'
-            ELSE '20-25 ly'
-        END AS distance_band
-    FROM star_systems_20_25_ly
-),
-ranked_bands AS (
-    SELECT
-        designation,
-        distance_ly,
-        constellation,
-        stellar_class,
-        distance_band,
-        ROW_NUMBER() OVER (PARTITION BY distance_band ORDER BY distance_ly) AS rank_in_band
-    FROM distance_bands
-)
+WITH
+    distance_bands AS (
+        SELECT
+            designation,
+            distance_ly,
+            constellation,
+            stellar_class,
+            CASE
+        WHEN distance_ly < 15 THEN '10-15 ly'
+        WHEN distance_ly < 20 THEN '15-20 ly'
+        WHEN distance_ly < 25 THEN '20-25 ly'
+        WHEN distance_ly < 30 THEN '25-30 ly'
+        ELSE '> 30 ly'
+    END AS distance_band
+        FROM star_systems_20_25_ly
+    ),
+    ranked_bands AS (
+        SELECT
+            designation,
+            distance_ly,
+            constellation,
+            stellar_class,
+            distance_band,
+            ROW_NUMBER() OVER (PARTITION BY distance_band ORDER BY distance_ly ASC) AS rank_in_band
+        FROM distance_bands
+    )
 SELECT
     distance_band,
     rank_in_band,
@@ -73,26 +76,34 @@ SELECT
     distance_ly,
     constellation
 FROM ranked_bands
-ORDER BY distance_band, rank_in_band;
+ORDER BY distance_band ASC, rank_in_band ASC;
 GO
 
 -- Query 7: Summary of stars per 5 ly band with statistics
-SELECT
-    CASE
+WITH
+    distance_bands AS (
+        SELECT
+            designation,
+            distance_ly,
+            constellation,
+            stellar_class,
+            CASE
         WHEN distance_ly < 15 THEN '10-15 ly'
         WHEN distance_ly < 20 THEN '15-20 ly'
-        ELSE '20-25 ly'
-    END AS distance_band,
+        WHEN distance_ly < 25 THEN '20-25 ly'
+        WHEN distance_ly < 30 THEN '25-30 ly'
+        ELSE '> 30 ly'
+    END AS distance_band
+        FROM star_systems_20_25_ly
+    )
+SELECT
+    distance_band,
     COUNT('*') AS star_count,
     ROUND(MIN(distance_ly), 1) AS min_distance,
     ROUND(MAX(distance_ly), 1) AS max_distance,
     ROUND(AVG(distance_ly), 1) AS avg_distance
-FROM star_systems_20_25_ly
-GROUP BY CASE
-    WHEN distance_ly < 15 THEN '10-15 ly'
-    WHEN distance_ly < 20 THEN '15-20 ly'
-    ELSE '20-25 ly'
-END
-ORDER BY min_distance;
+FROM distance_bands
+GROUP BY distance_band 
+ORDER BY star_count desc;
 GO
 
