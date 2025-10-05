50bb5ee974b1fbd3f8e25df3f611e992e2888980
TimelordUK <sjames8888@gmail.com>
2025-10-05 12:24:11 +0100

    color syntax for fix

--- nvim-plugin/examples/sql-cli.lua
+++ nvim-plugin/examples/sql-cli.lua
@@ -12,7 +12,60 @@ return
 			persist = true,       -- Enable auto save/load
 			max_items = 100,      -- Maximum queries to keep
 			auto_save = true,     -- Save after each query
-		}
+		},
+
+        -- FIX Message Syntax Highlighting
+        syntax = {
+          patterns = {
+            -- Message Types
+            { pattern = [[\<ExecutionReport\>]], group = "FixExecReport",
+              color = { gui = "#50fa7b", cterm = "Green", bold = true } },
+            { pattern = [[\<AllocationReport\>]], group = "FixAllocReport",
+              color = { gui = "#8be9fd", cterm = "Cyan", bold = true } },
+            { pattern = [[\<NewOrderSingle\>]], group = "FixNewOrder",
+              color = { gui = "#f1fa8c", cterm = "Yellow", bold = true } },
+            { pattern = [[\<OrderCancelReject\>]], group = "FixCancelReject",
+              color = { gui = "#ff5555", cterm = "Red", bold = true } },
+
+            -- Order Status
+            { pattern = [[\<PendingNew\>]], group = "FixStatPendingNew",
+              color = { gui = "#f1fa8c", cterm = "Yellow" } },
+            { pattern = [[\<New\>]], group = "FixStatNew",
+              color = { gui = "#50fa7b", cterm = "Green" } },
+            { pattern = [[\<PartiallyFilled\>]], group = "FixStatPartial",
+              color = { gui = "#50fa7b", cterm = "Green" } },
+            { pattern = [[\<Filled\>]], group = "FixStatFilled",
+              color = { gui = "#50fa7b", cterm = "Green", bold = true } },
+            { pattern = [[\<Canceled\>]], group = "FixStatCanceled",
+              color = { gui = "#6272a4", cterm = "DarkGray" } },
+            { pattern = [[\<Rejected\>]], group = "FixStatRejected",
+              color = { gui = "#ff5555", cterm = "Red", bold = true } },
+
+            -- Side (Buy/Sell)
+            { pattern = [[\<Buy\>]], group = "FixSideBuy",
+              color = { gui = "#50fa7b", cterm = "Green", bold = true } },
+            { pattern = [[\<Sell\>]], group = "FixSideSell",
+              color = { gui = "#ff5555", cterm = "Red", bold = true } },
+
+            -- Exchanges
+            { pattern = [[\<NYSE\>]], group = "FixExchNYSE",
+              color = { gui = "#bd93f9", cterm = "Magenta" } },
+            { pattern = [[\<NASDAQ\>]], group = "FixExchNASDAQ",
+              color = { gui = "#ff79c6", cterm = "Magenta" } },
+            { pattern = [[\<LSE\>]], group = "FixExchLSE",
+              color = { gui = "#bd93f9", cterm = "Magenta" } },
+
+            -- Instrument Types
+            { pattern = [[\<NDS\>]], group = "FixInstNDS",
+              color = { gui = "#00aaff", cterm = "Cyan" } },
+            { pattern = [[\<NFD\>]], group = "FixInstNFD",
+              color = { gui = "#ffaa00", cterm = "Yellow" } },
+            { pattern = [[\<CDS\>]], group = "FixInstCDS",
+              color = { gui = "#aa00ff", cterm = "Magenta" } },
+            { pattern = [[\<IRS\>]], group = "FixInstIRS",
+              color = { gui = "#00ffaa", cterm = "Green" } },
+          }
+        },
       })
 
       -- Token Manager Configuration
