676309f2510c5e86054f4dce7f43faa45d78e47c
TimelordUK <sjames8888@gmail.com>
2026-09-01 21:13:41 +0100

    fix a example sql

--- examples/statistical_analysis.sql
+++ examples/statistical_analysis.sql
@@ -107,42 +107,6 @@ ORDER BY occurrences DESC
 LIMIT 1;
 GO
 
--- Statistical summary for outlier detection
--- Values more than 2 standard deviations from mean might be outliers
--- Note: Since CROSS JOIN with CTEs has limitations, we calculate z-scores inline
-WITH sales_with_zscore AS (
-    SELECT
-        product,
-        region,
-        sales_amount,
-        (sales_amount - (SELECT AVG(sales_amount) FROM sales_data)) /
-              (SELECT STDDEV(sales_amount) FROM sales_data) as z_score_raw
-    FROM sales_data
-),
-with_classification AS (
-    SELECT
-        product,
-        region,
-        sales_amount,
-        ROUND(z_score_raw, 2) as z_score,
-        ABS(z_score_raw) as abs_z_score,
-        CASE
-            WHEN ABS(z_score_raw) > 2 THEN 'Potential Outlier'
-            WHEN ABS(z_score_raw) > 1 THEN 'Above/Below Average'
-            ELSE 'Normal Range'
-        END as classification
-    FROM sales_with_zscore
-)
-SELECT
-    product,
-    region,
-    sales_amount,
-    z_score,
-    classification
-FROM with_classification
-ORDER BY abs_z_score DESC
-LIMIT 10;
-GO
 
 -- Coefficient of Variation (CV) - relative variability measure
 -- CV = (Standard Deviation / Mean) * 100
