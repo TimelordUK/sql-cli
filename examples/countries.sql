18a563437c9fcefd18294db2a5e6cad2842f8e20
TimelordUK <sjames8888@gmail.com>
2026-09-12 20:20:06 +0100

    add county example

---
+++ examples/countries.sql
@@ -0,0 +1,46 @@
+
+WITH
+    countries AS (
+        SELECT *
+        FROM READ_CSV('data/countries.csv')
+    )
+SELECT
+ *
+INTO #countries
+FROM countries;
+go
+
+WITH
+    all AS (
+        SELECT *
+        FROM #countries
+    ),
+    codes AS (
+        SELECT value AS code
+        FROM SPLIT('AZE,BLR,CHN,EST,FIN,GEO,KAZ,PRK,LVA,LTU,MNG,NOR,POL,UKR', ',')
+    )
+SELECT
+    "name.common" AS name,
+    region,
+    cca3,
+    currencies,
+    capital,
+    latlng,
+    area,
+    callingCodes,
+    borders,
+    landlocked,
+    code
+FROM all
+INNER JOIN codes ON all.cca3 = codes.code
+ORDER BY name ASC;
+GO
+
+SELECT
+    region,
+    COUNT('*') AS n
+FROM #countries
+GROUP BY region
+ORDER BY n desc, region ASC;
+GO
+
